81380d15469a67c83bb9e5ad5d32294429bea293
Herman Groenenboom <taartaar@gmail.com>
2025-09-25 10:22:43 +0200

    Added DisableOnStart component
    Added MouseRaycaster2D which can handle 2d raycasts with custom collision layer filtering on gameobjects implementing IMouseHandler

---
+++ Basics/Input/MouseRaycaster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Basics
+{
+    public class MouseRaycaster2D : MonoBehaviour
+    {
+        [SerializeField]
+        private LayerMask layerMask = int.MaxValue;
+        public int mouseButton = 0;
+
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(mouseButton))
+            {
+                var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                var hits = Physics2D.OverlapPointAll(mousePos, layerMask.value);
+                foreach(var hit in hits)
+                {
+                    IMouseHandler handler = hit.GetComponent<IMouseHandler>();
+                    if(handler != null)
+                    {
+                        handler?.OnMouseClick(mouseButton);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
